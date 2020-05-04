39599627fbee164155f0daf47e1d769d29439128
Max <max22dev@gmail.com>
2020-05-04 19:43:44 +0300

    enabled disabled audio/video

--- app/components/video-chat/publisher/index.js
+++ app/components/video-chat/publisher/index.js
@@ -1,9 +1,12 @@
 import Component from '@ember/component';
 import { tracked } from '@glimmer/tracking';
+import {action} from '@ember/object';
 
 export default class VideoChatPublisherComponent extends Component {
     tagName = '';
     @tracked stream = null;
+    @tracked isEnabledAudio = true;
+    @tracked isEnabledVideo = true;
 
     get videoElement(){
         return this.getVideoEl(this.stream);
@@ -28,5 +31,23 @@ export default class VideoChatPublisherComponent extends Component {
         // videoElement.play();
 
         return videoElement;
+    }
+
+    @action
+    onMuteVideo(){
+        let videoTracks = this.stream.getVideoTracks();
+        this.isEnabledVideo = !this.isEnabledVideo;
+        videoTracks.forEach((track) =>{
+            track.enabled = this.isEnabledVideo;
+        })
+    }
+
+    @action
+    onMuteAudio(){
+        let audioTracks = this.stream.getAudioTracks();
+        this.isEnabledAudio = !this.isEnabledAudio;
+        audioTracks.forEach((track) =>{
+            track.enabled = this.isEnabledAudio;
+        })
     }
 }
